e2cbf6c711471b7d02c095b8b1985e7fa8169a8e
djpexhibit <visionx.prasad@gmail.com>
2020-11-03 10:30:52 +0800

    auth - allow some paths

--- auth/src/main/java/com/bestSearch/auth/AuthConfig.java
+++ auth/src/main/java/com/bestSearch/auth/AuthConfig.java
@@ -1,22 +1,25 @@
+package com.bestSearch.auth;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
-//@Configuration
+@Configuration
   public class AuthConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
       http
           .authorizeRequests(authz -> authz
-//              .antMatchers("/bestsearch-service/actuator**").permitAll()
+              .antMatchers("/actuator/**").permitAll()
+//              .antMatchers("/api/v1/organization-type/**").permitAll()
 //              .antMatchers(HttpMethod.GET, "api/v1/organization-type/test/**").hasAuthority("SCOPE_read")
 //              .antMatchers(HttpMethod.POST, "/test").hasAuthority("SCOPE_write")
-//              .anyRequest().authenticated())
-              .anyRequest().permitAll());
-//          .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+              .anyRequest().authenticated())
+//              .anyRequest().permitAll());
+          .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
   }
 
--- auth/src/main/java/com/bestSearch/auth/TestController.java
+++ auth/src/main/java/com/bestSearch/auth/TestController.java
@@ -1,3 +1,5 @@
+package com.bestSearch.auth;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
