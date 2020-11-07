195d76fca743a4adcdf3584a3129799d6d0a9499
supun_g <supun.g@synergenhealth.com>
2020-11-07 00:28:07 -0600

    restructured app

---
+++ organization/src/main/java/com/bestSearch/organization/OrganizationApp.java
@@ -0,0 +1,12 @@
+package com.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizationApp.class, args);
+    }
+}
